faa4c0ad73e9d6348171d57834e7443b02e50677
1amsahil <sahil981156@gmail.com>
2025-01-18 13:57:56 +0530

    Function Basics

---
+++ 8Function.js
@@ -0,0 +1,19 @@
+// Function
+function greeting()
+{
+    console.log("Hello ! World")
+}
+
+// Function with Parameters
+function multiplication(a, b)
+{
+    return a*b;
+};
+console.log(multiplication(5,3));
+
+// Arrow Function
+var print = (a,b)=>
+{
+    return a*b;
+};
+console.log(print(5,2));
